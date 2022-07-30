d8eeb7ae46b30e2d927d42ffd7dae4f763fabdd4
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-07-30 18:49:17 +0300

    good

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Box } from './box/Box';
+import { Container, Title } from './App.styled';
+import { useState, useEffect, useMemo } from 'react';
+import { ContactForm } from './form/Form';
+import ContactList from './contacts/ContactsList';
+import { nanoid } from 'nanoid';
+import { Filter } from './filter/Filter';
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? initialValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  return [value, setValue];
 };
+
+export default function App() {
+  const initialContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
+  const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
+
+  const [filter, setFilter] = useState('');
+
+  const submitForm = ({ name, number }, { resetForm }) => {
+    const contactsNames = contacts.map(contact => contact.name);
+    if (contactsNames.includes(name)) {
+      alert(` ${name} is already in contacts.`);
+    } else {
+      const person = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      setContacts(prevState => [...prevState, person]);
+      resetForm();
+    }
+  };
+
+  const newContacts = useMemo(() => {
+    return contacts.filter(person =>
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [contacts, filter]);
+
+  const onFilter = evt => {
+    setFilter(evt.target.value);
+  };
+
+  const onDelete = id => {
+    setContacts(prevState => {
+      return prevState.filter(c => c.id !== id);
+    });
+  };
+
+  return (
+    <Box
+      as={'div'}
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      color="white"
+      backgroundColor="#1d2327"
+    >
+      <Container>
+        <Title> Phonebook </Title>
+        <ContactForm submitForm={submitForm} />
+        <Filter value={filter} onFilter={onFilter} />
+        <ContactList contactsInfo={newContacts} deleteContact={onDelete} />
+      </Container>
+    </Box>
+  );
+}
---
+++ src/components/theme.js
@@ -0,0 +1,44 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#FFF',
+    text: '#2a2a2a',
+    primary: '#07c',
+    background: 'rgb(48, 58, 64)',
+    secondary: '#05a',
+    accent: '#B0C4DE',
+    accentText: 'rgb(122, 180, 180)',
+    muted: '#f6f6f6',
+    nice: 'rgb(10, 148, 134)',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '5px',
+    round: '50%',
+  },
+};
