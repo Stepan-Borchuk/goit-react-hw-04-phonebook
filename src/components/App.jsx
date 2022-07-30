13f405fae04b9df8ad7fc666bdda349d028e5711
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-07-30 18:52:22 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ const useLocalStorage = (key, initialValue) => {
 
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(value));
-  }, [value]);
+  }, [value, key]);
 
   return [value, setValue];
 };
